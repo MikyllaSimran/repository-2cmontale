a4d2ed085a39a1b33f3d38e5a76fbfde3d5f4045
MikyllaSimran <mikylla.delrosario@iismontalegenova.it>
2024-11-19 09:41:38 +0100

    for

--- programma.js
+++ programma.js
@@ -1,30 +1,42 @@
+let idrogeno = {};
+idrogeno.numero = 1;
+idrogeno.simbolo = 'H';
+idrogeno.nome = 'idrogeno'
+idrogeno.wiki = 'https://it.wikipedia.ohttps://it.wikipedia.org/wiki/Idrogenorg/wiki/Pagina_principale';
+
+let elio = {};
+elio.numero = 2;
+elio.simbolo = 'He';
+elio.nome ='elio';
+elio.wiki = 'https://it.wikipedia.org/wiki/Elio://it.wikipedia.org/wiki/Pagina_principale';
 
 
-let contenitore =document.getElementById('contenitore');
+let elementi = [idrogeno, elio];
 
+for (const element of elementi) {
+    let contenitore =document.getElementById('contenitore');
 contenitore.style.borderStyle = 'solid';
 
 let elemento = document.createElement('div');
 elemento.style.borderStyle = 'solid'
 elemento.style.width = '70px'
 elemento.style.textAlign = 'center'
-elemento.style.position = 'absolute'
-elemento.style.top = '500px';
-elemento.style.left = '500px';
+
 
 let numeroAtomico = document.createElement('p');
-numeroAtomico.innerText = 1;
+numeroAtomico.innerText = element.numero;
 elemento.appendChild(numeroAtomico);
 
 let simbolo = document.createElement('a');
-simbolo.innerText = 'H';
-simbolo.href = 'https://it.wikipedia.org/wiki/Pagina_principale'
+simbolo.innerText = element.simbolo;
+simbolo.href = elemento.wiki
 simbolo.style.fontSize = '25px'
 elemento.appendChild(simbolo);
 
 
 let nome = document.createElement('p');
-nome.innerText = 'idrogeno';
+nome.innerText = element.nome;
 elemento.appendChild(nome);
 
 contenitore.appendChild(elemento);
+}
